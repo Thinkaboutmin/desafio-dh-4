fb0a0982c86d6b6a94680f8ee4ea720b2fd67c9e
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2021-01-25 16:29:47 -0300

    Add splash

---
+++ app/src/main/java/com/example/dhdesafio4/ui/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.dhdesafio4.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.dhdesafio4.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private val binding: ActivitySplashBinding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val handle = Handler(Looper.myLooper()!!)
+        handle.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, 5000)
+    }
+}
