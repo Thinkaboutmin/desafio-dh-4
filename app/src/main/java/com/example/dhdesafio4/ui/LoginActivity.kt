3e865684071454bfd5ec0acf8ca243186be8e06c
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2021-01-27 00:10:15 -0300

    Use only the fragment as root

--- app/src/main/java/com/example/dhdesafio4/ui/LoginActivity.kt
+++ app/src/main/java/com/example/dhdesafio4/ui/LoginActivity.kt
@@ -3,16 +3,11 @@ package com.example.dhdesafio4.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dhdesafio4.R
-import com.example.dhdesafio4.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private val binding by lazy {
-        ActivityLoginBinding.inflate(layoutInflater)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_login)
     }
 
 }
